c80f41415fa0a7502c0dff857aa6395099154f7b
jay <jayjag04@gmai.com>
2021-02-09 07:35:44 -0500

    after challenge 15

---
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -0,0 +1,70 @@
+import { api, LightningElement, wire } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
+
+// imports
+// import getSimilarBoats
+import getSimilarBoats from "@salesforce/apex/BoatDataService.getSimilarBoats";
+
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
+  // Private
+  currentBoat;
+  relatedBoats;
+  boatId;
+  error;
+
+  // public
+  @api
+  get recordId() {
+    // returns the boatId
+    return this.boatId;
+  }
+
+  set recordId(value) {
+    console.log(value);
+    // sets the boatId attribute
+    this.setAttribute("boatId", value);
+    // sets the boatId value
+    this.boatId = value;
+  }
+
+  // public
+  @api
+  similarBy;
+
+  // Wire custom Apex call, using the import named getSimilarBoats
+  // Populates the relatedBoats list
+  @wire(getSimilarBoats, {
+    boatId: "$boatId",
+    similarBy: "$similarBy"
+  })
+  similarBoats({ error, data }) {
+    this.relatedBoats = undefined;
+    this.error = undefined;
+    if (data) {
+      this.relatedBoats = data;
+      console.log(data);
+    }
+    if (error) {
+      this.error = error;
+      console.log(error);
+    }
+  }
+  get getTitle() {
+    return "Similar boats by " + this.similarBy;
+  }
+  get noBoats() {
+    return !(this.relatedBoats && this.relatedBoats.length > 0);
+  }
+
+  // Navigate to record page
+  openBoatDetailPage(event) {
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: event.detail.boatId,
+        objectApiName: "Boat__c",
+        actionName: "view"
+      }
+    });
+  }
+}
